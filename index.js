ee10a56371cfea5b6f0f00c3ec58a6e231b98c15
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-22 20:31:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+//console.log(returnFirstTwoDrivers())
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num1){
+    return function(fareMultiply){
+        
+        return num1 * fareMultiply;
+    }
+}
+//console.log(createFareMultiplier())
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers)
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
